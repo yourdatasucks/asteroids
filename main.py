e4b88b9d4e993411965e0ffbe7fa6e92fb1b5bbf
yourdatasucks <2204530+yourdatasucks@users.noreply.github.com>
2024-08-25 15:53:12 -0400

    create player

--- main.py
+++ main.py
@@ -3,15 +3,22 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    delta_time = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        delta_time = timer.tick(60)/1000
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
