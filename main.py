c30b96bb36ce326ca1086b239274ee66e3f5d7db
yourdatasucks <2204530+yourdatasucks@users.noreply.github.com>
2024-08-25 11:42:21 -0400

    initial setup

---
+++ main.py
@@ -0,0 +1,10 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+def main():
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
